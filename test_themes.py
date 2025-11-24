feb8f6791607206eefecb83b666df462ce8c50e0
Wren Hawthorne <wren.hawthorne@madetech.com>
2025-11-24 11:38:34 +0000

    refactor naming, add additional assertions

--- test_themes.py
+++ test_themes.py
@@ -1,4 +1,10 @@
-from themes import x2
-def testIt():
-    #assert len(x2)>10
-    assert True is True
+from themes import duties_list
+
+def test_duties_list():
+    duty1 = "Duty 1 Script and code in at least one general purpose language and at least one domain-specific language to orchestrate infrastructure, follow test driven development and ensure appropriate test coverage."
+    duty13 = "Duty 13 Accept ownership of changes; embody the DevOps culture of 'you build it, you run it', with a relentless focus on the user experience."
+    
+    assert len(duties_list) > 10
+    assert len(duties_list) == 13
+    assert duties_list[0] == duty1
+    assert duties_list[-1] == duty13
