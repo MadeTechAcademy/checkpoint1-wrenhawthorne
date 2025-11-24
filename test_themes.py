e9fe0f8a1e4a3c94d2099251bc2c4b93cdeddd5e
Wren Hawthorne <wren.hawthorne@madetech.com>
2025-11-24 14:32:00 +0000

    roll back some changes

--- test_themes.py
+++ test_themes.py
@@ -1,18 +1,12 @@
-from themes import duties_list, create_html_output
+from themes import duties_list
 
-def test_duties_list():
+def test_duties_list_length():
+    assert len(duties_list) > 10
+    assert len(duties_list) == 13
+
+def test_duties_list_detail():
     duty1 = "Duty 1 Script and code in at least one general purpose language and at least one domain-specific language to orchestrate infrastructure, follow test driven development and ensure appropriate test coverage."
     duty13 = "Duty 13 Accept ownership of changes; embody the DevOps culture of 'you build it, you run it', with a relentless focus on the user experience."
     
-    assert len(duties_list) > 10
-    assert len(duties_list) == 13
     assert duties_list[0] == duty1
     assert duties_list[-1] == duty13
-
-def test_create_simple_html_output():
-    simple_html = """<html>
-        <head></head>
-        <body><p>Hello World!</p></body>
-        </html>"""
-
-    assert create_html_output()
