47491940d427180f5cd746ce2fc06da9aa755549
Wren Hawthorne <wren.hawthorne@madetech.com>
2025-11-25 10:27:36 +0000

    failing test for using duties in html template

--- test_apprenticeship.py
+++ test_apprenticeship.py
@@ -27,5 +27,11 @@ class TestOutputHtml:
 
         assert appr.output_html() == htmlTemplate
 
-    def test_outputs_to_html_document(self):
-        ...
+    def test_inserts_duties_as_paragraphs(self):
+        appr = Apprenticeship()
+        appr._duties = ['Focus on learning', 'Look after yourself', 'Read the road signs in Geoguessr and just lock in a guess early']
+
+        html = appr.output_html()
+
+        assert "<p>Focus on learning</p>" in html
+        assert "<p>Look after yourself</p>" in html
