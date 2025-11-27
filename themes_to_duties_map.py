9208c6027d0547a1ef46bb7a31245867601b8aa5
Wren Hawthorne <wren.hawthorne@madetech.com>
2025-11-27 17:22:45 +0000

    deeper theme duties working

--- themes_to_duties_map.py
+++ themes_to_duties_map.py
@@ -2,5 +2,6 @@ themes_to_duties_map = {
     'apprenticeship': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
     'bootcamp': [1, 2, 3, 4, 13],
     'automate': [5, 7, 10],
-    'houston': [6, 7, 10, 12]
+    'houston': [6, 7, 10, 12],
+    'deeper': [11]
 }
